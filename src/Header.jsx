32c7b7b79f91bff2c9c5b5083254548fe8f7d6e2
Asantagada <89428342+Asantagada@users.noreply.github.com>
2021-12-21 15:54:53 -0300

    Entrega Desafio menu

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header(){
+    return(
+        <div>
+            <h1 className="Titulo">Ecommerce</h1>
+            <NavBar></NavBar>
+        </div>)
+}
+export default Header
