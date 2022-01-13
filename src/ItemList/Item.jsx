5e1a8070a1576ee9434739d431fd9162948d6a15
Asantagada <89428342+Asantagada@users.noreply.github.com>
2022-01-13 20:27:00 -0300

    Primer entrega proyecto Final

--- src/ItemList/Item.jsx
+++ src/ItemList/Item.jsx
@@ -8,8 +8,8 @@ const Item = ({producto}) => {
             <h3>{producto.precio}</h3>
             <h3>{producto.stock}</h3>
             <img src={producto.foto} alt={producto.nombre}/>
-            <Link>
-                <button type='button' to={`/detalle/${producto.id}`}>Ver Detalle</button>
+            <Link to={`/detalle/${producto.id}`}>
+                <button type='button' >Ver Detalle</button>
             </Link>
         </div>
     )
